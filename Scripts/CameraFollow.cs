1fb2b7b062732e54ead07ae50a9ffeca5cff3e62
the-ict <dvltinv@gmail.com>
2025-12-19 22:06:33 +0500

    fix: Adjust initial camera pitch.

--- Scripts/CameraFollow.cs
+++ Scripts/CameraFollow.cs
@@ -6,7 +6,7 @@ public class CameraFollow : MonoBehaviour
 
     void Start() {
 	    transform.position = new Vector3(target.position.x, target.position.y + 1.7f, target.position.z + 0.5f);
-	    transform.rotation = Quaternion.Euler(30,180,0);
+	    transform.rotation = Quaternion.Euler(34,180,0);
     }
     void Update() {	
  	    transform.position = new Vector3(target.position.x, target.position.y + 3.5f, target.position.z +2f);
